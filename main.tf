358f1d269b5ba5ba3c87cbf80170a84eeeb02f58
Sebastian Poxhofer <secustor@users.noreply.github.com>
2019-12-17 19:14:16 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,10 @@
+module "foo" {
+  source  = "github.com/terraform-aws-modules/terraform-aws-eks?ref=v6.0.0"
+}
+module "consul" {
+  source    = "hashicorp/consul/aws"
+  version   = "0.1.0"
+}
+provider "azurerm" {
+  version = "1.36.1"
+}
